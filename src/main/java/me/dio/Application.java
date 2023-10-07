39a7fe4ece77463fe66a53ecbe628ce66d73dcf2
eiandressacs <andressa160r@gmail.com>
2023-10-07 18:38:17 -0300

    Initial commit

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,18 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/**
+ *  Iniciando RESTful API. */
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
